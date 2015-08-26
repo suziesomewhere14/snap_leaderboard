7eefcff711914096df050a4401214d51794fabd7
Akshay Karle <akshay.a.karle@gmail.com>
2015-08-26 18:37:07 -0300

    replace sqlite with pg to make heroku happy

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150826205219) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "feeds", force: :cascade do |t|
     t.string   "name"
     t.text     "message"
