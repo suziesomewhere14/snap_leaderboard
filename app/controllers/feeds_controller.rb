bca983f57a85adeec8c322b83aa592d5f6b2e512
Henrique Ribeiro Rezende <henriquerrezende@gmail.com>
2015-08-27 12:06:59 -0300

    Change ordering to use create at timestamp instead of update

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,7 +5,7 @@ class FeedsController < ApplicationController
   # GET /feeds
   # GET /feeds.json
   def index
-    @feeds = Feed.all.reverse
+    @feeds = Feed.all.order('created_at DESC')
   end
 
   # GET /feeds/1
